99e4e270a0fb24b5080fcfb848a3a520f17490dc
Alen Miskovic <92217202+alenmiskovic@users.noreply.github.com>
2023-08-09 09:35:25 +0200

    'main.js'

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import axios from 'axios';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-createApp(App).mount('#app')
+axios.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers['Authorization'] = 'Bearer ' + token;
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+createApp(App)
+    .use(router)
+    .use(store)
+    .mount('#app');
