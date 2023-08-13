67b8da81d65e03c83fff083cf70bf05d45ef52cd
Alen Miskovic <92217202+alenmiskovic@users.noreply.github.com>
2023-08-13 12:12:36 +0200

    'notfound'

---
+++ src/components/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="not-found-container">
+        <h1>Page Not Found</h1>
+        <p>The page you're looking for does not exist.</p>
+    </div>
+</template>
+
+<style scoped>
+    .not-found-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        background-color: rgba(255, 0, 0, 0.5);
+        color: white;
+    }
+</style>
