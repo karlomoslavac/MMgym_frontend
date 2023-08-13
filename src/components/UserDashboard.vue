8e9aea28d8692ab234d08f8b0396b9c49793b4a7
Alen Miskovic <92217202+alenmiskovic@users.noreply.github.com>
2023-08-13 11:49:16 +0200

    'user'

---
+++ src/components/UserDashboard.vue
@@ -0,0 +1,192 @@
+﻿<template>
+    <div class="dashboard-container">
+        <h1 class="dashboard-title">USER DASHBOARD</h1>
+        <p v-if="confirmationMessage" class="confirmation-message">{{ confirmationMessage }}</p>
+
+        <div class="selection-container">
+            <div class="selection-box">
+                <h2 class="section-title">Select a Gym</h2>
+                <select v-model="selectedGym" @change="fetchTrainers">
+                    <option disabled value="">Please select a gym</option>
+                    <option v-for="gym in gyms" :key="gym._id" :value="gym._id">
+                        {{ gym.name }}
+                    </option>
+                </select>
+            </div>
+
+            <div class="selection-box">
+                <h2 class="section-title">Select a Trainer</h2>
+                <select v-model="selectedTrainer" @change="fetchAppointments" :disabled="!selectedGym">
+                    <option disabled value="">Please select a trainer</option>
+                    <option v-for="trainer in trainers" :key="trainer._id" :value="trainer._id">
+                        {{ trainer.name }}
+                    </option>
+                </select>
+            </div>
+
+            <div class="selection-box">
+                <h2 class="section-title">Select a Date/Time</h2>
+                <select v-model="selectedAppointment" :disabled="!selectedTrainer">
+                    <option disabled value="">Please select a date/time</option>
+                    <option v-for="appointment in appointments" :key="appointment._id" :value="appointment._id">
+                        {{ appointment.date }}
+                    </option>
+                </select>
+            </div>
+        </div>
+
+        <button @click="confirmAppointment" class="confirm-button" :disabled="!selectedAppointment">Confirm Appointment</button>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        data() {
+            return {
+                gyms: [],
+                trainers: [],
+                appointments: [],
+                selectedGym: null,
+                selectedTrainer: null,
+                selectedAppointment: null,
+                errorMessage: null,
+                confirmationMessage: null
+            };
+        },
+        async created() {
+            try {
+                const response = await axios.get('http://localhost:3000/gyms');
+                this.gyms = response.data;
+                console.log('Gyms:', this.gyms); // Dodano
+            } catch (error) {
+                console.log('Error in created:', error); // Dodano
+                this.errorMessage = 'An error occurred. Please try again.';
+            }
+        },
+        methods: {
+            async fetchTrainers() {
+                try {
+                    const response = await axios.get(`http://localhost:3000/trainers/${this.selectedGym}/trainers`);
+                    this.trainers = response.data;
+                    console.log('Trainers:', this.trainers); // Dodano
+                } catch (error) {
+                    console.log('Error in fetchTrainers:', error); // Dodano
+                    this.errorMessage = 'An error occurred. Please try again.';
+                }
+            },
+            async fetchAppointments() {
+                try {
+                    console.log(this.selectedTrainer)
+                    const response = await axios.get(`http://localhost:3000/appointments`);
+                    const allAppointments = response.data;
+                    this.appointments = allAppointments.filter(appointment => appointment.trainer._id === this.selectedTrainer);
+                    console.log('Appointments:', this.appointments); // Dodano
+                } catch (error) {
+                    console.log('Error in fetchAppointments:', error); // Dodano
+                    this.errorMessage = 'An error occurred. Please try again.';
+                }
+            },
+            async confirmAppointment() {
+                try {
+                    // Ovdje biste napravili POST zahtjev na va� API za stvaranje sastanka
+                    // await axios.post('http://localhost:3000/appointments', { ... });
+
+                    const trainer = this.trainers.find(t => t._id === this.selectedTrainer);
+                    const gym = this.gyms.find(g => g._id === this.selectedGym);
+                    const appointment = this.appointments.find(a => a._id === this.selectedAppointment);
+
+                    if (trainer && gym && appointment) {
+                        console.log(`Confirmed appointment with ${trainer.name} at ${gym.name} on ${appointment.date}`);
+                        this.confirmationMessage = `You have successfully booked an appointment with ${trainer.name} at ${gym.name} on ${appointment.date}.`;
+                    } else {
+                        console.log('Error: Trainer, gym or appointment not selected');
+                        this.errorMessage = 'Please select a trainer, gym and appointment.';
+                    }
+                } catch (error) {
+                    console.log('Error in confirmAppointment:', error);
+                    this.errorMessage = 'An error occurred. Please try again.';
+                }
+            }
+        }
+    };
+</script>
+
+<style scoped>
+    .dashboard-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: calc(100% - 60px);
+    }
+
+    .selection-container {
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+        max-width: 800px;
+        margin-bottom: 20px;
+        padding-top: 60px;
+    }
+
+    .selection-box {
+        flex: 1;
+        margin: 0 10px 20px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .section {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 10px;
+        border-radius: 5px;
+        margin-bottom: 20px;
+        justify-content: center;
+    }
+
+    .sections-container {
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+        padding: 0 20px;
+        margin-bottom: 60px;
+    }
+
+    .dashboard-title,
+    .section-title {
+        width: fit-content;
+        text-align: center;
+        color: white;
+        background-color: rgba(0, 0, 0, 0.77);
+        padding: 10px;
+        border-radius: 5px;
+        margin-bottom: 20px;
+    }
+
+    .error-message {
+        color: red;
+        padding: 10px;
+        border-radius: 5px;
+    }
+
+    .confirmation-message {
+        color: green;
+        padding: 10px;
+        border-radius: 5px;
+    }
+
+    .confirm-button {
+        padding: 10px 20px;
+        border-radius: 5px;
+        background-color: #007BFF;
+        color: white;
+        cursor: pointer;
+        margin-top: 20px;
+    }
+</style>
