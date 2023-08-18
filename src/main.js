e134813835afef7c9888512839dc24c914353e95
alenmiskovic <92217202+alenmiskovic@users.noreply.github.com>
2023-08-18 15:04:48 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import router from './router';
 import store from './store';
 
+axios.defaults.baseURL = process.env.BACKEND_URL;
+
 axios.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
     if (token) {
