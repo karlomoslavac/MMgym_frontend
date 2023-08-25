dba87795f46c842247c470e094c8b4f02bd03fa7
karlomoslavac <115507336+karlomoslavac@users.noreply.github.com>
2023-08-25 17:26:47 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
         <div id="navbar">
             <h1>MM GYM</h1>
         </div>
-        <router-view></router-view>
+        <div id="main-content">
+            <router-view></router-view>
+        </div>
     </div>
 </template>
 
@@ -18,11 +20,10 @@
         height: 100%;
         margin: 0;
         padding: 0;
-        overflow: hidden;
     }
 
     #app {
-        height: 100%;
+        min-height: 100vh;
         font-family: Avenir, Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
@@ -31,16 +32,23 @@
         background-image: url(../public/background.jpg);
         background-size: cover;
         background-position: center;
+        background-attachment: fixed;
     }
 
     #navbar {
         position: fixed;
         top: 0;
         width: 100%;
-        background-color: rgba(0, 0, 0, 0.5); 
+        background-color: rgba(0, 0, 0, 0.5);
         color: white;
         padding: 10px 0;
         text-align: center;
-        z-index: 1000; 
+        z-index: 1000;
+    }
+
+    #main-content {
+        padding-top: 150px; 
+        min-height: auto; 
+        overflow-y: auto; 
     }
 </style>
